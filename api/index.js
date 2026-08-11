a224c073d6e22e82cf378fcc69cb5b2c684179e6
Charles Chirenje <charles1chirenje@gmail.com>
2026-08-11 21:47:40 +0200

    Fix import path in api/index.js to target server/server.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@
 // Express app directly - every request under /api/* is routed here by
 // vercel.json, and this file simply exports the same `app` used for local
 // development in server/server.js.
-const app = require('../server/app');
+const app = require('../server/server.js');
 
 module.exports = app;
